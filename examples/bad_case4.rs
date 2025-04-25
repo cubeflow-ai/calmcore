2e5d1aaa50956888659601f2a0147de199d47eca
Ansj <ansj-sun@163.com>
2025-04-25 17:32:55 +0800

    up

--- examples/bad_case4.rs
+++ examples/bad_case4.rs
@@ -1,27 +1,48 @@
 use std::{sync::Mutex, time::Duration};
 
+use croaring::{bitmap, Bitmap};
+
 struct Test {
     lock: Mutex<()>,
 }
 
 pub fn main() {
-    let test = Test {
-        lock: Mutex::new(()),
-    };
+    let mut b1 = Bitmap::new();
+    let mut b2 = Bitmap::new();
 
-    test.t1();
-    test.t2();
-}
-
-impl Test {
-    fn t1(&self) {
-        let _lock = self.lock.lock().unwrap();
-        println!("t1");
-        self.t2();
+    for i in 0..4_000_000 {
+        if i % 2 == 0 {
+            b1.add(i);
+        }
     }
 
-    fn t2(&self) {
-        let _lock = self.lock.lock().unwrap();
-        println!("t2");
+    for i in 5_000_000..5_000_000 {
+        if i % 2 == 0 {
+            b2.add(i);
+        }
     }
+
+    let start = std::time::Instant::now();
+    let mut sum = 0;
+    for b in b1.iter() {
+        if b == 0 {
+            sum += b;
+        }
+    }
+    for b in b2.iter() {
+        if b == 0 {
+            sum += b;
+        }
+    }
+    println!("use time : {} sum:{}", start.elapsed().as_millis(), sum);
+
+    let start = std::time::Instant::now();
+    let b3 = b1 & b2;
+    let mut sum = 0;
+    for b in b3.iter() {
+        if b == 0 {
+            sum += b;
+        }
+    }
+    println!("use time : {} sum:{}", start.elapsed().as_millis(), sum);
 }
